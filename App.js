4cdc15f06dd38d1a2a9138cb4a6c2595e9534707
virendra-978 <virendrat2121@gmail.com>
2024-10-16 19:16:24 +0530

    updated

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.get("/",(res,res) => {
+app.get("/",(req,res) => {
     res.send("<h1> Hello World </h1>");
 });
 app.listen(port,()=>{
